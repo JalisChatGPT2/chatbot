bcc603048eaf156afb01c138ec5e8f750cfe422b
JalisseChatGPT6 <artisan2020@hotmail.com>
2026-03-06 19:28:09 +0100

    Comment out unused getUser import
    Comment out the import statement for getUser from db.

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -1,7 +1,7 @@
 import { NextAuth } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { compare } from "bcrypt-ts";
-import { getUser } from "./db";
+//import { getUser } from "./db";
 
 export const authOptions = {
   providers: [
